123b59c0edc196149bb8f7c5c751e3fcd56f1aee
Arash alone boy <titanteam0098@gmail.com>
2016-04-27 17:47:58 +0430

    Create avast_member.lua

---
+++ plugins/avast_member.lua
@@ -0,0 +1,27 @@
+do
+ function run(msg, matches)
+ then
+return [[
+@arashnomiri
+@AllwenWaker
+@S_poker_S
+@Oo_abolHT_marginam_oO
+@Hossein_avast
+@aLiReZa_RmZz
+@iq_plus
+@M_Silent_mafia_iS_DeaD_M
+@Mersad_nph
+@Best_hacker_in_world
+@redteam_01_01
+@Iam_Poker
+@Mr_Anti_Admin
+]]
+end
+end
+return {
+patterns = {
+"^(اعضا تیم آواست)$",
+},
+run = run
+}
+end
