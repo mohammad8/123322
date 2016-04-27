009b54dfdb7c59ed7f3c48678bb73b79e2f352eb
Arash alone boy <titanteam0098@gmail.com>
2016-04-27 17:39:28 +0430

    Create helps.lua
    update!

---
+++ plugins/helps.lua
@@ -0,0 +1,26 @@
+do
+ function run(msg, matches)
+if is_momod(msg) then
+return [[ 
+راهنما کامل ابزار های ربات آواست:
+
+1-برای دریافت لیست ساده مدیریت گروه
+ !help
+●●●
+2-برای دریافت راهنما پلاگین ها دستور
+!helppl
+●●●
+4-برای دریافت لیست مرجع ابزار ها دستور
+helps
+●●●
+>>برای اطلاعات بیشتر در کانال @avast_Team عضو شوید.
+]]
+end
+end
+return {
+patterns = {
+"^([Hh][Ee][Ll][Pp][Ss])$",
+},
+run = run
+}
+end
